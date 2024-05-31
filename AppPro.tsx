c58b1e420b991f0cdb6f495585a57634b173cb50
Kapil Arora <2754963a@student.gla.ac.uk>
2024-05-31 10:45:17 +0530

    Lets toggle between dark mode and light mode.

---
+++ AppPro.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Text, View, StyleSheet, useColorScheme } from 'react-native'
+
+function AppPro(): JSX.Element {
+
+    const isDarkMode = useColorScheme() === 'dark';
+
+    return (
+        <View style={styles.container}>
+            <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+                Hello Ji
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    whiteText: {
+        color: '#FFFFFF',
+        fontSize: 50
+    },
+    darkText: {
+        color: '#000000',
+        fontSize: 50
+    }
+})
+
+export default AppPro
